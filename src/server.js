0140ee93f10a270a61c0385beb970718469f187c
Sávio Silva <165408521+iToskka@users.noreply.github.com>
2024-05-20 14:37:47 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -18,5 +18,6 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
+  console.log("Boas-vindas à API do AllBooks")
   console.log("Acesse esta API em http://localhost:8000")
 })
